fec4d6373de91f2de215fdf980340cb304185a8c
Pete Burch <peteburch223@gmail>
2016-03-07 18:39:31 +0000

    refactored test in line with walkthrough

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,20 +3,19 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard){ described_class.new }
 
-  TOP_AMOUNT = 10
+  TOP_UP_AMOUNT = 10
 
-  it {is_expected.to respond_to(:balance)}
-
-  it 'has an initial balance of zero' do
-    expect(oystercard.balance).to eq(0)
+  context 'initial tests' do
+    it {is_expected.to respond_to(:balance)}
+    it {is_expected.to respond_to(:top_up).with(1).argument}
+    it 'has an initial balance of zero' do
+      expect(oystercard.balance).to eq(0)
+    end
   end
 
-  it {is_expected.to respond_to(:top_up).with(1).argument}
-
-  it 'balance increases when money is added' do
-    oystercard.top_up(TOP_AMOUNT)
-    expect(oystercard.balance).to eq TOP_AMOUNT
+  describe '#top_up' do
+    it 'balance increases when money is added' do
+      expect {oystercard.top_up(TOP_UP_AMOUNT)}.to change{oystercard.balance}.by(TOP_UP_AMOUNT)
+    end
   end
-
-
 end
