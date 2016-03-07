3ad331394e35574ec9b991d239ac848321103fbd
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-07 18:29:01 +0000

    challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,22 @@
 require 'oystercard'
 
-describe OysterCard do
+describe Oystercard do
   subject(:oystercard){ described_class.new }
+
+  TOP_AMOUNT = 10
 
   it {is_expected.to respond_to(:balance)}
 
   it 'has an initial balance of zero' do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
+
+  it {is_expected.to respond_to(:top_up).with(1).argument}
+
+  it 'balance increases when money is added' do
+    oystercard.top_up(TOP_AMOUNT)
+    expect(oystercard.balance).to eq TOP_AMOUNT
+  end
+
+
 end
