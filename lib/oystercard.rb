c7bd9e602ad64a748bfc2f8329e9cac63af075de
Hanif Fakira <fakira.work@gmail.com>
2016-03-08 10:37:17 +0000

    Q4 Done

---
+++ lib/oystercard.rb
@@ -0,0 +1,20 @@
+class Oystercard
+  #attr_reader :balance
+
+  DEFAULT_BALANCE = 0
+
+  def initialize
+    @balance = DEFAULT_BALANCE
+  end
+
+  def zero_balance?
+    start_balance
+  end
+
+  private
+
+  def start_balance
+    @balance == DEFAULT_BALANCE
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  subject(:oystercard) {described_class.new}
+  describe 'new card' do
+    it 'with balance of £0' do
+      expect(oystercard).to be_zero_balance
+    end
+  end
 end
